d89ca333e10d555f76cde722f7cf8dfe608ee7b3
Lucas Acelino <95665852+lucasacelino@users.noreply.github.com>
2022-11-06 11:22:16 -0300

    Update index.js

--- Semana5_Atividade2_Q1/index.js
+++ Semana5_Atividade2_Q1/index.js
@@ -1,20 +1,34 @@
-let nomes = [
-    'Maria benedita da SilvA',
-    'joão ricardo alburquerque',
-    'JOSÉ de Arimatéia',
-];
+const lista = ["Maria benedita da SilvA", "joão ricardo albuquerque", "JOSÉ de Arimatéia"]
+const listaAuxiliar = []
+const listaCorrigida = []
 
-let updatelist = [];
-
-for (index = 0; index < nomes.length; index++ ){
-    nome = nomes[index].toLowerCase();
-    //console.log(nome);
-    updatelist.push(nome);
-    console.log(updatelist);
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
 
- {
+for (let i = 0; i < lista.length; i++) {
+
+    //transforma todos os caracteres em minúsculo
+    nome = lista[i]
+    nome = nome.toLowerCase();
+    listaAuxiliar.push(nome);
+
+    //transforma o primeiro caractere em maiúsculo
+    nome = listaAuxiliar[i].split(" ");
+
+    //percorre a lista com os nomes separados
+    nomeCompletoCorrigido = []
+    for (let j = 0; j < nome.length; j++) {
+        
+        if ((nome[j] == "de") || (nome[j] == "da")) {
+            nomeCompletoCorrigido += nome[j] + " "
+        } else {
+            nomeCompletoCorrigido += nome[j].replaceAt(0, nome[j][0].toUpperCase()) + " "
+        }
+    }
+    //retira o espaço depois do fim do nome
+    listaCorrigida.push(nomeCompletoCorrigido.trim())
+    
 }
 
-
-
+console.log(listaCorrigida);
